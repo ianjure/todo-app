bc389ea4bdd89af39baefed031670cbdd9a697a9
ianjure <ianjuremacalisang2@gmail.com>
2025-02-21 10:02:00 +0800

    added the controllers and routes for creating a user or todo

--- src/server.js
+++ src/server.js
@@ -1,50 +1,13 @@
 const express = require("express");
-
 const connectDB = require("./config/db");
-
-const User = require("./models/user.model");
-const Todo = require("./models/todo.model");
+const userRouter = require("./routes/user.route");
+const todoRouter = require("./routes/todo.route");
 
 const app = express();
 
 app.use(express.json());
-
-// Route: POST /api/products - Create a Product
-app.post("/user/create", async (req, res) => {
-    const user = req.body;
-
-    if(!user.username || !user.password) {
-        return res.status(400).json({ success: false, message: "Please provide all fields." });
-    }
-
-    const newUser = new User(user); // create a new product with the data sent by the user
-
-    try {
-        await newUser.save(); // save the product to the database
-        res.status(201).json({ success: true, data: newUser });
-    } catch (error) {
-        console.error("Error in creating product: ", error.message);
-        res.status(500).json({ success: false, message: "Internal server error." });
-    }
-});
-
-app.post("/todo/create", async (req, res) => {
-    const todo = req.body;
-
-    if(!todo.description) {
-        return res.status(400).json({ success: false, message: "Please provide all fields." });
-    }
-
-    const newTodo = new Todo(todo); // create a new product with the data sent by the user
-
-    try {
-        await newTodo.save(); // save the product to the database
-        res.status(201).json({ success: true, data: newTodo });
-    } catch (error) {
-        console.error("Error in creating product: ", error.message);
-        res.status(500).json({ success: false, message: "Internal server error." });
-    }
-});
+app.use("/user", userRouter);
+app.use("/todo", todoRouter);
 
 connectDB((client) => {
     if (client) {
