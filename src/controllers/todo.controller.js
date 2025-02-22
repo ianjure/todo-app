5b4283baca3ae0c0ea74ea1e0caee74c93dfee29
ianjure <ianjuremacalisang2@gmail.com>
2025-02-22 14:33:51 +0800

    added todo controller

---
+++ src/controllers/todo.controller.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Todo = require("../models/todo.model");
+
+const createTodo = async (req, res) => {
+    // code here
+};
+
+const updateTodo = async (req, res) => {
+    // code here
+};
+
+const deleteTodo = async (req, res) => {
+    // code here
+};
+
+module.exports = { createTodo, updateTodo, deleteTodo };
