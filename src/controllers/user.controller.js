8c392298f08f7f9e94b4d11987e9ae26c927e3e4
ianjure <ianjuremacalisang2@gmail.com>
2025-02-22 14:35:41 +0800

    added user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const User = require("../models/user.model");
+
+const createUser = async (req, res) => {
+    // code here
+};
+
+const deleteUser = async (req, res) => {
+    // code here
+}
+
+module.exports = { createUser, deleteUser };
