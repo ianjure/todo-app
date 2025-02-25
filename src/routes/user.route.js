1347ae7a93b6adfee664622a85fb14a356b2bd02
ianjure <ianjuremacalisang2@gmail.com>
2025-02-25 08:37:33 +0800

    enabled login for testing

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getUsers, createUser, deleteUser } = require("../controllers/user.controller");
+const { getUsers, createUser, deleteUser, loginUser } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/api", createUser);
 router.delete("/api/:id", deleteUser);
+router.post("/api/login", loginUser);
 
 module.exports = router;
