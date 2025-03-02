98c33a2d696e157d6e36f62b6dcb438033165c4a
honeyangel3 <honeyangelhp@gmail.com>
2025-03-03 04:56:16 +0800

    create user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,27 @@ const getUsers = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-    // code here
+    try {
+        const { username, password } = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username already exists!" });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create and save user
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: "Account successfully created!" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Failed to create account! Please try again." });
+    }
 };
 
 const deleteUser = async (req, res) => {
