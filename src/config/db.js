124012917e8203102d718f5619d5b6d05eeca39e
ianjure <ianjuremacalisang2@gmail.com>
2025-03-02 14:33:46 +0800

    added a few comments

--- src/config/db.js
+++ src/config/db.js
@@ -5,13 +5,16 @@ dotenv.config();
 
 const connectDB = async (callback) => {
     try {
+        // Connect to MongoDB
         const client = await mongoose.connect(process.env.MONGO_URI);
         
+        // Check if the client is connected
         if (client) {
             console.log(`MongoDB Connected: ${client.connection.host}`);
             return callback(client);
         }
 
+        // If the client is not connected, return null
         return callback(null);
 
     } catch (error) {
