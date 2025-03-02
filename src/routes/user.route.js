8ec96779df50a2f4a2ef58012f1d5b94b57ce68f
ianjure <ianjuremacalisang2@gmail.com>
2025-03-03 07:45:26 +0800

    added get status route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/auth.middleware");
 const { signupUser, loginUser } = require("../controllers/auth.controller");
-const { getUsers } = require("../controllers/user.controller");
+const { getLeaderboard, getStatus } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
-router.get('/', authMiddleware, getUsers);
+router.get('/leaderboard', authMiddleware, getLeaderboard);
+router.get('/', authMiddleware, getStatus);
 
 module.exports = router;
