c27521aaae9427bc5a98dee1ea2c470f01709b6d
ianjure <ianjuremacalisang2@gmail.com>
2025-03-03 10:45:23 +0800

    server changes

--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static('public'));
 app.use('/api/admin', adminRouter);
 app.use("/api/user", userRouter);
 app.use("/api/task", taskRouter);
