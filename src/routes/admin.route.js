733f9ef4487bf7954165569845eaeb4806d3dc68
ianjure <ianjuremacalisang2@gmail.com>
2025-03-03 10:43:19 +0800

    added logout to admin

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/auth.middleware");
-const { loginAdmin, signupAdmin } = require("../controllers/auth.controller");
+const { loginAdmin, signupAdmin, logout } = require("../controllers/auth.controller");
 const { getUsers, deleteUser } = require("../controllers/admin.controller");
 
 const router = express.Router();
 
 router.post("/signup", signupAdmin);
 router.post("/login", loginAdmin);
+router.post("/logout", logout);
 router.get("/users", authMiddleware, getUsers);
 router.delete("/users/:id", authMiddleware, deleteUser);
 
