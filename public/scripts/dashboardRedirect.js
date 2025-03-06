583fae0260be7ea72e041f0397746ca16f40b79b
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-03-06 10:28:41 +0800

    Update dashboardRedirect.js

--- public/scripts/dashboardRedirect.js
+++ public/scripts/dashboardRedirect.js
@@ -1,14 +1,14 @@
 function dashboardRedirect() {
-  // Get the token, username, and role from the local storage
-  const token = localStorage.getItem("token");
-  const username = localStorage.getItem("username");
-  const role = localStorage.getItem("role");
-  
-  // Redirect to dashboard if token, username, or role exists
-  if (token || username || role) {
-      window.location.replace(`/${username}`);
-      return;
-  }
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+    const role = localStorage.getItem("role");
+      
+    // Redirect to dashboard if token, username, or role exists
+    if (token || username || role) {
+        window.location.replace(`/${username}`);
+        return;
+    }
 }
 
+// Run on page load
 dashboardRedirect();
